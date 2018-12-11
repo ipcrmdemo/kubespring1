a68f03d37251b7acd7d7025fc97db7a87fa6bc5a
Matt Cadorette <mcadorette@gmail.com>
2018-12-11 08:00:58 -0800

    Initial commit from Atomist

---
+++ src/main/java/com/kubespring1/Kubespring1Application.java
@@ -0,0 +1,12 @@
+package com.kubespring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kubespring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kubespring1Application.class, args);
+	}
+}
